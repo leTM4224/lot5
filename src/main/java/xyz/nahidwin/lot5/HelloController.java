54a4e094068b49b8d4a474785a37edf31e4fd478
Arcthuruss <73853221+Arcthuruss@users.noreply.github.com>
2024-06-04 13:52:50 +0200

    nah, I'd win.

--- src/main/java/xyz/nahidwin/lot5/HelloController.java
+++ src/main/java/xyz/nahidwin/lot5/HelloController.java
@@ -6,14 +6,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 
 public class HelloController {
-    @FXML private Label welcomeText;
     @FXML private Button btn;
     @FXML private ImageView img;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     protected void pressed() {
